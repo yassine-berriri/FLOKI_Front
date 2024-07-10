92dc2c15800cc0e207e35c803dd2fcce4ff5cf21
yassineBr12 <yassine.berriri@etu.univ-cotedazur.fr>
2024-07-11 00:10:02 +0200

    add transporter form

--- src/app/core/views/signup/signup.component.ts
+++ src/app/core/views/signup/signup.component.ts
@@ -1,15 +1,37 @@
 import { Component, signal, Signal } from '@angular/core';
 import { SharedModule } from '../../../shared/shared.module';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatStepperModule} from '@angular/material/stepper';
+import {MatButtonModule} from '@angular/material/button';
+import {FormBuilder, Validators, FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [SharedModule],
+  imports: [SharedModule,
+    MatButtonModule,
+    MatStepperModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+  ],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.scss'
 })
 export class SignupComponent {
   userType = signal<string | null>('sender');
+
+  firstFormGroup = this._formBuilder.group({
+    firstCtrl: ['', Validators.required],
+  });
+  secondFormGroup = this._formBuilder.group({
+    secondCtrl: ['', Validators.required],
+  });
+  isLinear = false;
+
+  constructor(private _formBuilder: FormBuilder) {}
   
   selectUserType(userType: string) {
     this.userType.set(userType);
